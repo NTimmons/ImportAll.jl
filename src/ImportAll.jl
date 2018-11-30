a56eb60f8de02eb6c913a673d67c68361243d395
Nicholas Timmons <ngtimmons@gmail.com>
2018-11-30 15:23:27 +0000

    Removed debug code

--- src/ImportAll.jl
+++ src/ImportAll.jl
@@ -4,20 +4,13 @@ macro importall(moduleIn, verbose=false)
     modName = @eval($(moduleIn))
     out = []
     for name in names(modName)
-        #if(name == :exp || name == :sqrt || name == :+)
             symbolname = @eval $(name)
-            #expression  = Expr(:import, modName.symbolname) 
             expression = :(import $(moduleIn): $(name))
             push!(out, expression)
-        #end
-        
     end
-    #@show out
+	
     ret = Expr(:block,out...)
 	return ret
-    #return quote
-    #        $out
-    #end
 end
 
 export @importall
